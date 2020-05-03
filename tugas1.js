83366c22aedcc4db002aaee4e8700a3fab97e491
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 14:34:08 +0700

    TUGAS 1 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas1.js
@@ -0,0 +1,17 @@
+var message = "Ilham";
+let msg1 = "Mansur";
+
+const msg2 = 27;
+
+
+// if(true){
+// 	var message = "gagah";
+// 	let msg1 = "memang";
+// 	console.log("Saya", msg1)
+// }
+
+
+console.log(message)
+console.log(msg1)
+console.log(msg2)
+
